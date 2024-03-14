ce064a2b1bf6ef4444f96efdf5f4984e04f9da03
Jeet <rahulcse1@gmail.com>
2024-03-14 17:26:10 +0100

    updated annotation example

--- src/main/java/com/tw/thinkcode/year24/AnnotationExample.java
+++ src/main/java/com/tw/thinkcode/year24/AnnotationExample.java
@@ -1,17 +1,55 @@
 package com.tw.thinkcode.year24;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-@interface MyAnnotation {
-    String value() default "Hello World!";
-    int count() default 1;
+import lombok.Data;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@interface JsonSerializable {
 }
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@interface JsonElement {
+    public String key() default "";
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface Init {
+}
+
+@JsonSerializable
+@Data
+class Person {
+
+    @JsonElement
+    private String firstName;
+
+    @JsonElement
+    private String lastName;
+
+    @JsonElement(key = "personAge")
+    private String age;
+
+    private String address;
+
+    @Init
+    private void initNames() {
+        this.firstName = this.firstName.substring(0, 1).toUpperCase() 
+          + this.firstName.substring(1);
+        this.lastName = this.lastName.substring(0, 1).toUpperCase() 
+          + this.lastName.substring(1);
+    }
+}
+
+
 public class AnnotationExample {
-    @MyAnnotation(value = "Hello, this is a test", count = 2)
-    static void testAnnotation() {
-        System.out.println("This is a test method");
-    }
-    public static void main(String[] args) {
-        testAnnotation();
-    }
+   public static void main(String[] args) {
+
+   }
 }
